e36a6b4e749d68d86184889055bb1fffb32f94a2
mrunix00 <mrunix00@protonmail.com>
2023-03-25 21:53:41 +0100

    Fix compilation for linux

--- src/read.h
+++ src/read.h
@@ -1,9 +1,14 @@
 #include <errno.h>
-#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifdef linux
+#include <linux/limits.h>
+#else
+#include <limits.h>
+#endif
 
 char	**read_command();
 void	  free_command(char **cmd);
